7cba7dbd6c5c65f0a24f3f7b85b707546322b7c2
oliverhering8080 <82911864+oliverhering8080@users.noreply.github.com>
2021-12-04 13:30:05 -0800

    Add files via upload

---
+++ BUAN Project 3.R
@@ -0,0 +1,64 @@
+# BUAN 4310 Data Mining and Big Data
+# Tinh-an Le, Zack Carey, Oliver Hering
+# Project 3
+
+library(rpart)
+library(rpart.plot)
+library(forecast)
+library(caret)
+
+
+houses <- read.csv('house_3.csv', header = TRUE)
+
+head(houses)
+str(houses)
+t(t(names(houses)))
+
+# removing unneeded variables
+houses <- houses[, c(8:20, 7)]
+t(t(names(houses)))
+
+
+# Training Validation split
+set.seed(666)
+
+train_index <- sample(1:nrow(houses), 0.6 * nrow(houses))
+valid_index <- setdiff(1:nrow(houses), train_index)
+
+train_df <- houses[train_index, ]
+valid_df <- houses[valid_index, ]
+
+nrow(train_df)
+nrow(valid_df)
+
+str(train_df)
+str(valid_df)
+
+# Regression Tree
+tree <- rpart(price ~ ., data = train_df, method = 'anova')
+prp(tree)
+
+
+printcp(tree)
+plotcp(tree)
+
+
+# Predicting train_df and valid_df
+predict_train <- predict(tree, train_df)
+accuracy(predict_train, train_df$price)
+
+predict_valid <- predict(tree, valid_df)
+accuracy(predict_valid, valid_df$price)
+
+
+# Predicting new records
+houses_test <- read.csv('house_test_3.csv', header = TRUE)
+t(t(names(houses_test)))
+
+houses_test <- houses_test[, c(7:19)]
+t(t(names(houses_test)))
+
+houses_test_prediction <- predict(tree, newdata = houses_test)
+houses_test_prediction
+
+
